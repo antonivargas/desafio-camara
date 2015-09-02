ed7a4205972e7bc678c476cba3d811bc9722b868
Dimi Antoni <antonivargas@gmail.com>
2015-09-02 14:37:01 -0300

    tramento amigável de erros

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -57,7 +57,6 @@ class Admin extends CI_Controller {
 
         if($this->input->post()){
 
-
             $arquivo = $this->input->post();
 
             //var_dump($arquivo);exit();
@@ -70,59 +69,85 @@ class Admin extends CI_Controller {
             }
 
             $ler = $link . $ar;
+
             //var_dump($ler);exit();
+            
+            @$xml = simplexml_load_file($ler);
 
-            $xml = simplexml_load_file($ler);
+            //var_dump($xml);exit();
 
-            foreach($xml-> proposicao as $prop){
-        
-                $id     = utf8_decode($prop->id);     
-                $nome   = utf8_decode($prop->nome);
-                $idTipoProposicao   = utf8_decode($prop->tipoProposicao->id);
-                $sigla   = utf8_decode($prop->tipoProposicao->sigla);
-                $numero = utf8_decode($prop->numero);
-                $ano    = utf8_decode($prop->ano);
-                $idOrgaoNum     = utf8_decode($prop->orgaoNumerador->id);
-                $siglaOrgaoNum  = utf8_decode($prop->orgaoNumerador->sigla);
-                $dataApresentacao   = utf8_decode($prop->datApresentacao);
-                $txtEmenta      = utf8_decode($prop->txtEmenta);
-                $ExplicacaoEmta = utf8_decode($prop->txtExplicacaoEmenta);
-                $codRegime      = utf8_decode($prop->regime->codRegime);
-                $txtRegime      = utf8_decode($prop->regime->txtRegime);
-                $idApreciacao   = utf8_decode($prop->apreciacao->id);
-                $txtApreciacao  = utf8_decode($prop->apreciacao->txtApreciacao);
-                $nomeAutor      = utf8_decode($prop->autor1->txtNomeAutor);
-                $ideCadastro    = utf8_decode($prop->autor1->ideCadastro);
-                $codPartido     = utf8_decode($prop->autor1->codPartido);
-                $siglaPartido   = utf8_decode($prop->autor1->txtSiglaPartido);
-                $siglaUF        = utf8_decode($prop->autor1->txtSiglaUF);  
-                $qtdAutores     = utf8_decode($prop->qtdAutores);
-                $datDespacho    = utf8_decode($prop->ultimoDespacho->datDespacho);
-                $txtDespacho    = utf8_decode($prop->ultimoDespacho->txtDespacho);
-                $idSituacao     = utf8_decode($prop->situacao->id);
-                $situacaoDesc   = utf8_decode($prop->situacao->descricao);
-                $codOrgaoEstado     = utf8_decode($prop->situacao->orgao->codOrgaoEstado);
-                $siglaOrgaoEstado   = utf8_decode($prop->situacao->orgao->siglaOrgaoEstado);
-                $codProposicaoPrincipal = utf8_decode($prop->situacao->principal->codProposicaoPrincipal);
-                $proposicaoPrincipal    = utf8_decode($prop->situacao->principal->proposicaoPrincipal);   
-                $indGenero  = utf8_decode($prop->indGenero);
-                $qtdOrgaosComEstado = utf8_decode($prop->qtdOrgaosComEstado);
-                $this->load->model('proposicoes_model');
-                $retorno['resposta'] = $this->proposicoes_model->atualizar($id,$nome,$idTipoProposicao,$sigla,  
-                $numero, $ano, $idOrgaoNum,$siglaOrgaoNum,$dataApresentacao,$txtEmenta,$ExplicacaoEmta,$codRegime,
-                $txtRegime,$idApreciacao,$txtApreciacao,$nomeAutor,$ideCadastro,$codPartido,$siglaPartido,$siglaUF,
-                $qtdAutores,$datDespacho,$txtDespacho,$idSituacao,$situacaoDesc,$codOrgaoEstado,$siglaOrgaoEstado, 
-                $codProposicaoPrincipal,$proposicaoPrincipal,$indGenero,$qtdOrgaosComEstado);               
+            if($xml != null){
 
-            }
+                foreach(@$xml->proposicao as $prop){            
+                    $id     = utf8_decode($prop->id);     
+                    $nome   = utf8_decode($prop->nome);
+                    $idTipoProposicao   = utf8_decode($prop->tipoProposicao->id);
+                    $sigla   = utf8_decode($prop->tipoProposicao->sigla);
+                    $numero = utf8_decode($prop->numero);
+                    $ano    = utf8_decode($prop->ano);
+                    $idOrgaoNum     = utf8_decode($prop->orgaoNumerador->id);
+                    $siglaOrgaoNum  = utf8_decode($prop->orgaoNumerador->sigla);
+                    $dataApresentacao   = utf8_decode($prop->datApresentacao);
+                    $txtEmenta      = utf8_decode($prop->txtEmenta);
+                    $ExplicacaoEmta = utf8_decode($prop->txtExplicacaoEmenta);
+                    $codRegime      = utf8_decode($prop->regime->codRegime);
+                    $txtRegime      = utf8_decode($prop->regime->txtRegime);
+                    $idApreciacao   = utf8_decode($prop->apreciacao->id);
+                    $txtApreciacao  = utf8_decode($prop->apreciacao->txtApreciacao);
+                    $nomeAutor      = utf8_decode($prop->autor1->txtNomeAutor);
+                    $ideCadastro    = utf8_decode($prop->autor1->ideCadastro);
+                    $codPartido     = utf8_decode($prop->autor1->codPartido);
+                    $siglaPartido   = utf8_decode($prop->autor1->txtSiglaPartido);
+                    $siglaUF        = utf8_decode($prop->autor1->txtSiglaUF);  
+                    $qtdAutores     = utf8_decode($prop->qtdAutores);
+                    $datDespacho    = utf8_decode($prop->ultimoDespacho->datDespacho);
+                    $txtDespacho    = utf8_decode($prop->ultimoDespacho->txtDespacho);
+                    $idSituacao     = utf8_decode($prop->situacao->id);
+                    $situacaoDesc   = utf8_decode($prop->situacao->descricao);
+                    $codOrgaoEstado     = utf8_decode($prop->situacao->orgao->codOrgaoEstado);
+                    $siglaOrgaoEstado   = utf8_decode($prop->situacao->orgao->siglaOrgaoEstado);
+                    $codProposicaoPrincipal = utf8_decode($prop->situacao->principal->codProposicaoPrincipal);
+                    $proposicaoPrincipal    = utf8_decode($prop->situacao->principal->proposicaoPrincipal);   
+                    $indGenero  = utf8_decode($prop->indGenero);
+                    $qtdOrgaosComEstado = utf8_decode($prop->qtdOrgaosComEstado);
 
-            if($retorno == true){
+                    //$sql = "";
+
+                    $this->load->model('proposicoes_model');
+                    $retorno['resposta'] = $this->proposicoes_model->atualizar($id,$nome,$idTipoProposicao,$sigla,  
+                    $numero, $ano, $idOrgaoNum,$siglaOrgaoNum,$dataApresentacao,$txtEmenta,$ExplicacaoEmta,$codRegime,
+                    $txtRegime,$idApreciacao,$txtApreciacao,$nomeAutor,$ideCadastro,$codPartido,$siglaPartido,$siglaUF,
+                    $qtdAutores,$datDespacho,$txtDespacho,$idSituacao,$situacaoDesc,$codOrgaoEstado,$siglaOrgaoEstado, 
+                    $codProposicaoPrincipal,$proposicaoPrincipal,$indGenero,$qtdOrgaosComEstado);               
+
+                }
+                if(@$retorno == true){
 
                 $msg['mensagem'] ='A base de dados foi atualizada com sucesso';
                 //var_dump($msg['mensagem']);exit();
+                $this->load->view('sistema/atualizou',$msg);
+                }else{
+                    $msg['mensagem'] ='Ocorreu um erro inesperado na leitura da base';
+                    $this->load->view('sistema/atualizou',$msg);            
+
+                }
+            }else{
+
+                if(@$retorno == true){
+                
+                //var_dump($retorno);exit;
+
+                    $msg['mensagem'] ='A base de dados foi atualizada com sucesso';
+                    //var_dump($msg['mensagem']);exit();
+                    $this->load->view('sistema/atualizou',$msg);
+                }else{
+                    $msg['mensagem'] ='Ocorreu um erro inesperado na leitura da base';
+                    $this->load->view('sistema/atualizou',$msg);            
+
+                }      
             }
 
-                $this->load->view('sistema/atualizou',$msg);
+
         }
 
 
--- application/views/sistema/atualizou.php
+++ application/views/sistema/atualizou.php
@@ -2,14 +2,23 @@
 
 <?php $this->load->view("include/menu_admin.php"); ?>
 
-<?php if(isset($mensagem)){ ?>
+<?php if(isset($mensagem) && ($mensagem == 'A base de dados foi atualizada com sucesso')){ ?>
 <div class="panel panel-success">
-      <div class="panel-heading">
-      <h3 class="panel-title">Atualizado!</h3>
-      </div>
-      <div class="panel-body">
-        <?php if($mensagem !== null){ echo $mensagem; } ?>
-      </div>
-    </div>
+	<div class="panel-heading">
+	<h3 class="panel-title">Atualizado!</h3>
+	</div>
+	<div class="panel-body">
+	<?php if($mensagem !== null){ echo $mensagem; } ?>
+	</div>
+</div>
+<?php } else{?>
+<div class="panel panel-warning">
+	<div class="panel-heading">
+	<h3 class="panel-title">Não foi possível atualizar!</h3>
+	</div>
+	<div class="panel-body">
+	<?php if($mensagem !== null){ echo $mensagem; } ?>
+	</div>
+</div>
 <?php } ?>
 <?php $this->load->view("include/adm_footer.php"); ?>
